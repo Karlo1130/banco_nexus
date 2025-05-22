b07cca8eeac2b4fdac4bf7d38ffdac2ea366a040
karlo <kaes_21@alu.uabcs.mx>
2025-05-21 21:13:39 -0700

    add depositos y retiros

--- controller/accountController.js
+++ controller/accountController.js
@@ -5,8 +5,12 @@ export const getAccount = async (req, res) => {
 
         const {cuenta} = req.params
         
+        const [transactions] = await pool.query(
+            `SELECT * FROM transacciones
+            WHERE cuenta_id = ?`,
+            [cuenta]
+        )
 
-        const [transactions] = await pool.query(`SELECT * FROM transacciones WHERE cuenta_origen = ?`, [cuenta])
         const [balance] = await pool.query(`SELECT * FROM cuentas WHERE cuenta_id = ?`, [cuenta])
 
         console.log(transactions);
@@ -20,5 +24,36 @@ export const getAccount = async (req, res) => {
     } catch (error) {
         console.error('Error al obtener las transacciones: ', error)
         res.status(500).send('Ha ocurrido un error interno')
+    }
+}
+
+export const postTransaction = async (req, res) => {
+
+    try {
+        const {
+            type,
+            value
+        } = req.body
+
+        const {cuenta} = req.params
+
+        await pool.query(
+            `INSERT INTO transacciones 
+            (cuenta_id, tipo, monto) VALUES (?, ?, ?)`,
+            [cuenta, type, value]
+        )
+
+        const updateQuery = type === 'Deposito' 
+            ? `UPDATE cuentas SET saldo = saldo + ? WHERE cuenta_id = ?`
+            : `UPDATE cuentas SET saldo = saldo - ? WHERE cuenta_id = ?`;
+
+        await pool.query(updateQuery, [value, cuenta]);
+
+        res.status(201).send('Transaccion validada')
+
+    } catch (error) {
+        console.error('Error al registrar la transaccion: ', error)
+        res.status(500).send('Ha ocurrido un error interno')
+        
     }
 }
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,11 +1,13 @@
 import {Router} from 'express'
 
 import {
-    getAccount
+    getAccount,
+    postTransaction
 } from '../controller/accountController.js'
 
 const router = Router()
 
 router.get('/api/cuenta/:cuenta', getAccount)
+router.post('/api/transaction/:cuenta', postTransaction)
 
 export default router
